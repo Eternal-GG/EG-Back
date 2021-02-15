119e96dd83c9425c74f7320b9975093216c61c78
jjh5887 <wjdrnjsgh159@naver.com>
2021-02-15 23:58:25 +0900

    EGB-16 application.properties 암호화

---
+++ src/main/java/me/powerarc/eternalgg/common/JasyptConfig.java
@@ -0,0 +1,26 @@
+package me.powerarc.eternalgg.common;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${key}")
+    String key;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(key);
+        config.setPoolSize(1);
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
